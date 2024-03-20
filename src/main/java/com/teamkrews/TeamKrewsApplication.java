e045b1205b5c78967b5e2138a8b9a9349978b082
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-03-20 19:00:23 +0900

    feat: 날짜 데이터 적용 어노테이션 추가

--- src/main/java/com/teamkrews/TeamKrewsApplication.java
+++ src/main/java/com/teamkrews/TeamKrewsApplication.java
@@ -1,15 +1,19 @@
 package com.teamkrews;
 
+import java.util.TimeZone;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication()
+@EnableJpaAuditing
+@SpringBootApplication
 public class TeamKrewsApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(TeamKrewsApplication.class, args);
     }
 
--- src/main/java/com/teamkrews/chat/model/Message.java
+++ src/main/java/com/teamkrews/chat/model/Message.java
@@ -2,23 +2,22 @@ package com.teamkrews.chat.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Table(name = "message")
+@Table(name = "messages")
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,5 +31,6 @@ public class Message {
     private String content;
 
     @Column(name = "created_at")
+    @CreatedDate
     private LocalDateTime createdAt;
 }
