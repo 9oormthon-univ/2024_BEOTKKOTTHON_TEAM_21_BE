ba6d4a85ac5e4d9530e056ee006743e64fec2b67
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-03-20 16:31:19 +0900

    fix: 워크스페이스 DB 수정

--- src/main/java/com/teamkrews/workspace/model/Workspace.java
+++ src/main/java/com/teamkrews/workspace/model/Workspace.java
@@ -10,10 +10,12 @@ import lombok.Setter;
 public class Workspace {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "workspace_id")
+    private Long workspaceId;
 
     private String teamName;
 
+    @Column(name = "workspace_uuid")
     private String workspaceUUID;
 
     private String profileImageUrl;
