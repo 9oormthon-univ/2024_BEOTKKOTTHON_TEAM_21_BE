2227383ce1b83b2fd40d3c22aa039428e8ab4388
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2024-03-20 19:00:40 +0900

    chore: 메시지 컨트롤러 Swagger 추가

--- src/main/java/com/teamkrews/chat/controller/MessageController.java
+++ src/main/java/com/teamkrews/chat/controller/MessageController.java
@@ -10,22 +10,23 @@ import com.teamkrews.chat.repository.ChatRoomMessageRepository;
 import com.teamkrews.chat.repository.ChatRoomRepository;
 import com.teamkrews.chat.repository.ChatRoomUserRepository;
 import com.teamkrews.chat.service.MessageService;
+import com.teamkrews.utill.ApiResponse;
 import com.teamkrews.workspace.model.Workspace;
 import com.teamkrews.workspace.repository.WorkspaceRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RequiredArgsConstructor
-@Controller
-@Slf4j
+@RestController
+@RequestMapping("/message")
 public class MessageController {
 
     private final SimpMessagingTemplate messagingTemplate;
@@ -35,10 +36,9 @@ public class MessageController {
     private final WorkspaceRepository workspaceRepository;
     private final ChatRoomMessageRepository chatRoomMessageRepository;
 
-
     // 특정 채팅방의 메시지 불러오기
-    @GetMapping("/message/list")
-    public ResponseEntity<ChatRoomMessage> getMessages(@AuthenticationPrincipal User user, @RequestParam String workspaceUUID, @RequestParam Long chatRoomId) {
+    @GetMapping("/list")
+    public ResponseEntity<ApiResponse<ChatRoomMessage>> getMessagesWithUserAndWorkspaceAndChatRoom(@AuthenticationPrincipal User user, @RequestParam String workspaceUUID, @RequestParam Long chatRoomId) {
 
         Workspace workspace = workspaceRepository.findByWorkspaceUUID(workspaceUUID).orElseThrow();
         ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow();
@@ -49,11 +49,11 @@ public class MessageController {
 
         ChatRoomMessage chatRoomMessage = messageService.getMessages(chatRoom2);
 
-        return ResponseEntity.ok(chatRoomMessage);
+        return ResponseEntity.ok(ApiResponse.success(chatRoomMessage));
     }
 
     // 메시지 전송 및 저장
-    @MessageMapping("/message") // /pub/message로 날린 데이터에 대해 /sub/chat/room + roomId 구독자들에게 메시지 전송
+    @MessageMapping("") // /pub/message로 날린 데이터에 대해 /sub/chat/room + chatRoomId 구독자들에게 메시지 전송
     public void sendAndSaveMessage(@Payload Message message) {
 
         ChatRoomMessage chatRoomMessage = chatRoomMessageRepository.findByMessage(message);
--- src/main/java/com/teamkrews/chat/service/MessageService.java
+++ src/main/java/com/teamkrews/chat/service/MessageService.java
@@ -1,14 +1,10 @@
 package com.teamkrews.chat.service;
 
-import com.teamkrews.User.model.User;
 import com.teamkrews.chat.model.ChatRoom;
 import com.teamkrews.chat.model.ChatRoomMessage;
-import com.teamkrews.chat.model.ChatRoomUser;
 import com.teamkrews.chat.model.Message;
 import com.teamkrews.chat.repository.ChatRoomMessageRepository;
-import com.teamkrews.chat.repository.ChatRoomUserRepository;
 import com.teamkrews.chat.repository.MessageRepository;
-import com.teamkrews.workspace.model.Workspace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,14 +13,8 @@ import org.springframework.stereotype.Service;
 public class MessageService {
 
     private final MessageRepository messageRepository;
-    private final ChatRoomUserRepository chatRoomUserRepository;
     private final ChatRoomMessageRepository chatRoomMessageRepository;
 
-//    // 메시지 저장
-//    public Message saveMessage() {
-//        chatRoomMessageRepository.save(chatRoomMessage);
-//        return null;
-//    }
 
     // 특정 유저의 특정 워크스페이스의 특정 채팅방의 메시지 조회
     public ChatRoomMessage getMessages(ChatRoom chatRoom) {
