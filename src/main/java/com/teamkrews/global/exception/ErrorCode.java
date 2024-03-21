60e0cb6d330a883b4fe28c5db21e2f95553b94c9
youjae <youjae0803@gmail.com>
2024-03-21 16:41:18 +0900

    feat: 채팅방 조회 실패 ErrorCode 추가

--- src/main/java/com/teamkrews/global/exception/ErrorCode.java
+++ src/main/java/com/teamkrews/global/exception/ErrorCode.java
@@ -13,7 +13,10 @@ public enum ErrorCode {
 
     WORKSPACE_NOT_FOUND(HttpStatus.NOT_FOUND, "2404", "해당 워크 스페이스를 찾을 수 없습니다."),
     USER_WORKSPACE_NOT_FOUND(HttpStatus.NOT_FOUND, "3404", "해당 유저 워크 스페이스를 찾을 수 없습니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "4404", "해당 사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "4404", "해당 사용자를 찾을 수 없습니다."),
+    CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "5404", "해당 채팅방을 찾을 수 없습니다.");
+
+
 
     private final HttpStatus status;
     private final String code;
