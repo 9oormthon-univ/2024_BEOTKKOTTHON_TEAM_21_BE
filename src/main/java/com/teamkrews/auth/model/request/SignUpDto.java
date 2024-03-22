4240656a99ea6c82c3d22d455017c156c6461bec
youjae <youjae0803@gmail.com>
2024-03-22 15:22:21 +0900

    fix: 패스워드 검증 조건 수정

--- src/main/java/com/teamkrews/auth/model/request/SignUpDto.java
+++ src/main/java/com/teamkrews/auth/model/request/SignUpDto.java
@@ -1,8 +1,8 @@
 package com.teamkrews.auth.model.request;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,7 +15,8 @@ public class SignUpDto {
     @NotNull
     private String loginId;
 
-    @Min(8)
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()]).{8,20}$",
+            message = "비밀번호는 숫자, 대문자, 소문자, 특수문자를 최소 1개 이상 포함한 8자 이상 20자 이하의 문자열이어야 합니다.")
     private String password;
 
     @NotNull
