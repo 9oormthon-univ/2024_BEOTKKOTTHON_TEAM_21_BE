170f04142c7d0f0e6d65f79214058738b8b7690f
youjae <youjae0803@gmail.com>
2024-03-24 05:13:09 +0900

    chore: etc

--- src/main/java/com/teamkrews/chatRoomUser/controller/ChatRoomUserController.java
+++ src/main/java/com/teamkrews/chatRoomUser/controller/ChatRoomUserController.java
@@ -15,6 +15,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+//tem
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/chatRoomUser")
